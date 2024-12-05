ff81261b4dc02d10e48640e09c1c05159147dbc3
lckmbr <luckambere@gmail.com>
2024-12-05 16:52:42 +0300

    implemented favorite screen

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -107,7 +107,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                     label="My trips"
                   />
                   <MenuItem
-                    onClick={() => {}}
+                    onClick={() => router.push("/favorites")}
                     label="My favorites"
                   />
                   <MenuItem
