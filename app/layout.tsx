1207861990c5d507bfcb7dcf5392e8ff4d8fd041
lckmbr <luckambere@gmail.com>
2024-12-06 08:24:29 +0300

    added site favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,23 @@ export default async function RootLayout({
     <html lang="en">
       <head>
         <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon_io/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon_io/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon_io/favicon-16x16.png"
+        />
+        <link
           rel="preconnect"
           href="https://fonts.googleapis.com"
         />
