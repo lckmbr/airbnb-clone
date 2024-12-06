cc8bd0ffccd0b5007814844538698868520add05
lckmbr <luckambere@gmail.com>
2024-12-06 07:27:48 +0300

    wrapped IParams type in app/listings/[listingId]/page.tsx within a promise

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -6,13 +6,13 @@ import EmptyState from "@/app/components/EmptyState";
 import ListingClient from "./ListingClient";
 import getReservations from "@/app/actions/getReservations";
 
-interface IParams {
-  listingId?: string;
-}
+type IParams = Promise<{ listingId?: string }>;
 
 const ListingPage = async ({ params }: { params: IParams }) => {
-  const listing = await getListingById(params);
-  const reservations = await getReservations(params);
+  const parameters = await params;
+
+  const listing = await getListingById(parameters);
+  const reservations = await getReservations(parameters);
   const currentUser = await getCurrentUser();
 
   if (!listing) {
